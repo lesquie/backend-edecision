a1ae26b741821bc412bcd91a3b0fdebead7bf959
lesquie <lucasefpro2@gmail.com>
2022-02-10 13:09:54 +0100

    Initialisation

---
+++ src/main/java/com/ipi/backendedecision/BackendEdecisionApplication.java
@@ -0,0 +1,13 @@
+package com.ipi.backendedecision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEdecisionApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEdecisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipi/backendedecision/BackendEdecisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipi.backendedecision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEdecisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
